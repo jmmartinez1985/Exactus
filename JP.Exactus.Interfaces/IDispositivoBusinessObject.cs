0225796eda60968856c9ffaa6d9087245f392fa9
Jose Martinez <stn_rules@hotmail.com>
2015-09-30 20:47:52 -0500

    Liberando Interfcaes

---
+++ JP.Exactus.Interfaces/IDispositivoBusinessObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP.Exactus.Interfaces
+{
+    public interface IDispositivoBusinessObject
+    {
+
+    }
+}
---
+++ JP.Exactus.Logic/DispositivoBusinessObject.cs
@@ -0,0 +1,14 @@
+﻿using JP.Exactus.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP.Exactus.Logic
+{
+    public class DispositivoBusinessObject : IDispositivoBusinessObject
+    {
+
+    }
+}
