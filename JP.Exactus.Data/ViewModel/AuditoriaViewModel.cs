314d3e090f174aa58c50d3d7836e62ca8529f52e
jimmygk <yimmyperez@yahoo.com>
2015-09-30 20:35:11 -0500

    new view models

---
+++ JP.Exactus.Data/ViewModel/AuditoriaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP.Exactus.Data.ViewModel
+{
+    public class AuditoriaViewModel
+    {
+        public int IDAuditoria { get; set; }
+        public int IDDispositivo { get; set; }
+        public string DescripcionAccion { get; set; }
+        public System.DateTime Fecha { get; set; }
+
+        public virtual Dispositivo Dispositivo { get; set; }
+    }
+}
---
+++ JP.Exactus.Data/ViewModel/OpcionesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP.Exactus.Data.ViewModel
+{
+    public class OpcionesViewModel
+    {
+        public int IDOpcion { get; set; }
+        public string DescripcionOpcion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
